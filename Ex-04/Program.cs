f242460eb9d244e8d8bc6e27807472ea7b18d0a2
Abdulhakim Mamu <69505770+hakimparoo@users.noreply.github.com>
2022-11-03 17:53:51 +0700

    lab6

---
+++ Ex-04/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace OS_Sync_01
+{
+    class Program  
+    {
+        private static string x = "";
+        private static int exitflag = 0;
+        static object _Lock = new object();
+        // Semaphore s = new Semaphore(1, 1);
+
+        static void ThReadX()
+        {
+            lock (_Lock)
+            {
+                while (exitflag == 0)
+                {   
+                    Monitor.Pulse(_Lock);
+                    Monitor.Wait(_Lock);
+                    if (exitflag == 1)
+                    {
+                        Console.WriteLine("Thread 1 exit");    
+                    }
+                    else
+                        Console.WriteLine("X = {0}", x);
+                }
+            }
+        }
+
+        static void ThWriteX()
+        {
+            string xx;
+            lock (_Lock)
+            {
+                while (exitflag == 0)
+                {
+                    Console.Write("Input: ");
+                    xx = Console.ReadLine();
+                    if (xx == "exit")
+                    {
+                        exitflag = 1;
+                        Monitor.Pulse(_Lock);
+                    }
+                    else
+                    {
+                        x = xx;
+                        Monitor.Pulse(_Lock);
+                        Monitor.Wait(_Lock);
+                    }
+                }
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Thread A = new Thread(ThReadX);
+            Thread B = new Thread(ThWriteX);
+
+            A.Start();
+            B.Start();
+
+
+        }
+    }
+}
---
+++ Ex-05/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace cv_lab
+{
+        class Program
+        {
+                private static string x = "";
+                private static int exitflag = 0;
+                private static int updateflag = 0;
+                private static object _lock = new Object();
+
+
+
+                static void ThReadX(object i)
+                {
+
+                        while (exitflag == 0)
+                        {
+                                lock (_lock)
+                                {
+                                        if (x != "exit" && updateflag == 1)
+                                        {
+                                                Monitor.Pulse(_lock);
+                                                
+                                                Console.WriteLine("***Thread {0} : x = {1}***", i, x);
+                                                updateflag = 0;
+                                                Monitor.Wait(_lock);
+                                        }
+                                }
+                        }
+                        Console.WriteLine("---Thread {0} exit---", i);
+                }
+
+                static void ThWriteX()
+                {
+                        string xx;
+                        lock (_lock)
+                        {
+                                while (exitflag == 0)
+                                {
+                                        Console.Write("Input: ");
+                                        xx = Console.ReadLine();
+                                        if (xx == "exit")
+                                        {
+                                                x = xx;
+                                                exitflag = 1;
+                                                Monitor.Pulse(_lock);
+                                                
+                                        }
+                                        else
+                                        {
+                                                x = xx;
+                                                updateflag = 1;
+                                                Monitor.Pulse(_lock);
+                                                Monitor.Wait(_lock);
+                                        }
+
+                                }
+                        }
+                }
+
+                static void Main()
+                {
+                        Thread A = new Thread(ThWriteX);
+                        Thread B = new Thread(ThReadX);
+                        Thread C = new Thread(ThReadX);
+                        Thread D = new Thread(ThReadX);
+
+                        A.Start();
+                        B.Start(1);
+                        C.Start(2);
+                        D.Start(3);
+                }
+        }
+}
